69dea247988e02e7a27bc8529752703abea33a8c
Ramish93 <ramishshah93@gmail.com>
2021-07-23 18:15:10 +0000

    added all function to main, accessed the last row of stock sheet to calculate surplus

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,14 +17,18 @@ def get_sales_data():
     """
     gets sales data in input
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:      
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input('enter your data here:')
-    sales_data = data_str.split(',')
+        data_str = input('enter your data here:')
+        sales_data = data_str.split(',')
 
-    validate_data(sales_data)
+        if validate_data(sales_data):
+            print('valid data.')
+            break
+    return sales_data
 
 def validate_data(values):
     """
@@ -35,6 +40,35 @@ def validate_data(values):
             raise ValueError(f'6 values needed you added {len(values)}')      
     except ValueError as e:
         print(f'invalid data {e}, please try again.\n')
+        return False
+    return True
 
+def update_sales_worksheet(data):
+    """
+    updated the google worksheet in last row with provided user data
+    """
+    print('updating worksheet')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('data updated successfully')
 
-get_sales_data()
+def calsulate_surplus_data(sales_row):
+    """
+    calculated the extra sandwiches made or thrown away.
+    """
+    print('calculating surplus data')
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)    
+
+def main():
+    """
+    calls all the functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    calsulate_surplus_data(sales_data)
+    update_sales_worksheet(sales_data)
+
+print('Welcome to love sandwiches data automation')
+main()
